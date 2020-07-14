4946abc05d9206260ffeb6fd31f70a506d379d29
Clayton Bell <clayton@ii.net>
2020-07-14 21:32:21 +1000

    Improved tests-as-documentation for you to read

--- spec/gitops_cmdb_spec.rb
+++ spec/gitops_cmdb_spec.rb
@@ -1,18 +1,73 @@
 context 'help on how to use this library' do
 
-  let(:result) do
-    GitopsCmdb.file_load('spec/fixtures/include_simplest/parent.yaml')
+  describe 'keep your YAML configs DRY, by "include:"-ing multiple YAML files together' do
+
+    let(:result) do
+      GitopsCmdb.file_load('spec/fixtures/include_simplest/parent.yaml')
+    end
+
+    it 'included files are deep merged' do
+      expect(result).to eq({
+        'key1' => 'parent',
+        'key2' => 'child',
+        'both' => 'parent'
+      })
+    end
+
+    it 'the "include:" key is removed from the final result' do
+      expect(result.has_key?('include')).to be(false)
+    end
   end
 
-  it 'load a yaml file, also load the "include" yaml files and deep merges the result' do
-    expect(result).to eq({
-      'key1' => 'parent',
-      'key2' => 'child',
-      'both' => 'parent'
-    })
+  describe 'mustache {{ }} templating can be used. Variable names are defined in the "variables:" hash' do
+
+    describe 'static values can be defined' do
+
+      let(:result) do
+        GitopsCmdb.file_load('spec/fixtures/variable_simple/file.yaml')
+      end
+
+      it 'and the value substituted into the {{ }} mustache placeholder' do
+        expect(result).to eq({
+          'file' => 'file',
+          'variable' => 'value value1 nice'
+        })
+      end
+
+      it 'the "variables:" key is removed from the final result' do
+        expect(result.has_key?('variables')).to be(false)
+      end
+
+    end
+
+    describe 'or use OS environment variables' do
+
+      after(:each) { ENV.delete('OS_ENVIRONMENT_VARIABLE') }
+
+      it 'myValue' do
+        ENV['OS_ENVIRONMENT_VARIABLE'] = 'myValue'
+
+        result = GitopsCmdb.file_load('spec/fixtures/variable_os_env/file.yaml')
+
+        expect(result).to eq({
+          'file' => 'file',
+          'variable' => 'value myValue nice'
+        })
+      end
+
+      it 'blah snoo foo' do
+        ENV['OS_ENVIRONMENT_VARIABLE'] = 'blah snoo foo'
+
+        result = GitopsCmdb.file_load('spec/fixtures/variable_os_env/file.yaml')
+
+        expect(result).to eq({
+          'file' => 'file',
+          'variable' => 'value blah snoo foo nice'
+        })
+      end
+
+    end
+
   end
 
-  it 'the "include" key is removed once all the files have been loaded' do
-    expect(result.has_key?('include')).to be(false)
-  end
 end
